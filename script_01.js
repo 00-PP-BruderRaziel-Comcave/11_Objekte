988ce4991abc106bf6e341aafd2cf7f3e350446c
Nico <nico.aldag@web.de>
2023-07-21 11:51:53 +0200

    Therie und Tests zu Objekten

--- script_01.js
+++ script_01.js
@@ -8,34 +8,58 @@
 // output("------------");
 
 
-// // nested arrays - Prinzip: [[[]]]
-// // nested arrays  - hier: [[],[]]
-// arr =   [
-//                 ["Ich","bin","Max"],
-//                 ["Ich","bin","Moritz"]
-//         ];
+// nested arrays - Prinzip: [[[]]]
+// nested arrays  - hier: [[],[]]
+arr =   [
+                ["Ich","bin","Max"],
+                ["Ich","bin","Moritz"]
+        ];
 
         
 // output(arr);
 // output(arr[0]);
 // output(arr[0][0]+ " " + arr[1][2]);
 // output(arr[1][0]);
+// output("------------------");
 
 // // nested loop
 // for (let i = 0; i < arr.length; i++) {
 //     for (let j = 0; j < arr[i].length; j++) {
 //         output("Index i: " + i + " Index j: " + j);
-//         output(arr[i][j]);       
+//         output(arr[i][j]);
 //     }
 // }
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = {
+    firstName: "Bashkim",
+    familyName: "Dauti",
+    salary:[120000,160000],
+    permission: true,
+    sayHello: function () {
+        return "Hallo, " + this.firstName+"!";
+    }
+
+};
 
 
+output(person);
+output(person["firstName"]);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+person.permission=false;
+output(person.permission);
+output(person.sayHello())
 
 
+// const text =    "Ich bin " + person.firstName + 
+//                 " und verdiene " + person.salary[1] +
+//                 " p.a.";
+
+//  output(text)
 
 
 /** Ausgabe */
